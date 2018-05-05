b49aeba89a06c019675a7a821aff8f8fe00551e1
Eddy Segura <eseguran@outlook.com>
2018-05-05 16:50:31 -0600

    ok

--- quiz3/ViewController.swift
+++ quiz3/ViewController.swift
@@ -10,8 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var preguntas = ["Programa usted en Java?", "Es Leningrado la capital de Rusia?", "Programa usted en Swift?", "Es la Paz la capital de Bolivia?", "Programa usted en Python?", "Bolivia tiene Mar?", "Programa usted en C++?", "Programa usted en Python?", "esto funciona?", "Programa usted en C#?", "Programa usted en PHP?", "Es de dia?", "sabe usted ingles?", "Sabe usted Aleman?", "Habla usted Italiano?", "Habla usted Ruso?", "tiene hambre?", "Conoce China?", "Le gusta la cerveza?", "Las rosas son rojas?"]
+ 
+    var respuestas = [String]()
+    var a = 1;
+ 
+    @IBAction func no(_ sender: UIButton) {
+        respuestas.append("no")
+        if a < preguntas.count {
+            label.text = preguntas[a];
+            a = a+1;}
+        if a == 20 {
+            sender.isEnabled = false
+            
+        }
+    }
+    
+    @IBOutlet weak var label: UILabel!
+    @IBAction func yes(_ sender: UIButton) {
+        
+        respuestas.append("Yes")
+        if a < preguntas.count {
+            label.text = preguntas[a];
+            a = a+1;}
+        if a == 20 {
+            sender.isEnabled = false
+            
+            
+        }
+    }
+    @IBAction func next(_ sender: UIButton) {
+     
+    
+            label.text = preguntas[0];
+        sender.setTitle("Resultados", for:[])
+        sender.isEnabled = false;
+ 
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +59,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
